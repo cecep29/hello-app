a87ab9725578d624e24cb0f2e3cc8348d1bb0b76
cecep29 <cecepefendi.c2p@gmail.com>
2020-10-06 16:52:42 +0700

    initial-commit

---
+++ lib/main.dart
@@ -0,0 +1,42 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatefulWidget {
+  @override
+  _MyAppState createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  int counter = 0;
+
+  void tekanTombol() {
+    setState(() {
+      counter = counter + 1;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('belajar stateful'),
+        ),
+        body: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              Text(
+                counter.toString(),
+                style: TextStyle(fontSize: 10 + counter.toDouble()),
+              ),
+              RaisedButton(
+                  child: Text('Tambah Bilangan'), onPressed: tekanTombol)
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
